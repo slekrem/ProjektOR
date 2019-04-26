b049d312efd2e380d8bcff6c7d7d6706b6d461dd
YlloNieR <30906472+YlloNieR@users.noreply.github.com>
2019-04-26 14:19:27 +0200

    paper-dropdown-menu und polyfill einsatz

--- src/my-view2.js
+++ src/my-view2.js
@@ -8,10 +8,13 @@
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
-import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
+import { PolymerElement, html } from '@polymer/polymer';
 import './shared-styles.js';
 
-import '@polymer/paper-checkbox/paper-checkbox.js';
+import '@polymer/paper-checkbox/paper-checkbox.js'; //paper-checkbox
+import '@polymer/paper-dropdown-menu/paper-dropdown-menu.js'; //paper-dropdown-menu
+import '@polymer/paper-item/paper-item.js'; //paper-dropdown-menu
+import '@polymer/paper-listbox/paper-listbox.js'; //paper-dropdown-menu
 
 class MyView2 extends PolymerElement {
   static get template() {
@@ -23,13 +26,29 @@ class MyView2 extends PolymerElement {
           padding: 10px;
         }
       </style>
-      <p><a target="_blank" rel="noopener noreferrer" href="https://www.webcomponents.org/author/PolymerElements">PolymerElements</a></p>
+      <p><a target="_blank" rel="noopener noreferrer" href="https://www.webcomponents.org/author/PolymerElements/paper-elements">PolymerElements</a></p>
       
+      <div class="card">
+        <h1>paper-input-elements</h1>
       <div class="card">
         <h1>paper-checkbox</h1>
         <paper-checkbox>Unchecked</paper-checkbox>
         <paper-checkbox checked>Checked</paper-checkbox>
         <paper-checkbox disabled>Disabled</paper-checkbox>
+      </div>
+      <div class="card">
+
+        <h1>paper-dropdown-menu</h1>
+
+        <paper-dropdown-menu label="Dinosaurs">
+        <paper-listbox slot="dropdown-content" class="dropdown-content">
+          <paper-item>allosaurus</paper-item>
+          <paper-item>brontosaurus</paper-item>
+          <paper-item>carcharodontosaurus</paper-item>
+          <paper-item>diplodocus</paper-item>
+        </paper-listbox>
+        </paper-dropdown-menu
+      
       </div>
       
       <div class="card">
